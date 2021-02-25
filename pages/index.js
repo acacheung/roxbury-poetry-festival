b71cba81088ea919fa8f03bc35ea2ea64d4d496f
Amanda Cheung <acacheung@gmail.com>
2021-02-25 13:13:40 +1300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Roxbury Poetry Festival</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Roxbury Poetry Festival</h1>
+      </main>
+      <Footer />
+    </div>
+  );
+}
