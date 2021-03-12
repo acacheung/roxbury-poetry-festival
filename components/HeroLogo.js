2f64db47613bf0318b25fa58836ede2a9d786bc4
Amanda Cheung <acacheung@gmail.com>
2021-03-12 13:28:21 +1300

    hero logo component

---
+++ components/HeroLogo.js
@@ -0,0 +1,75 @@
+export default function HeroLogo() {
+  return (
+    <div className="flex flex-col">
+      <div className="flex logo-roxbury justify-center">
+        <svg height="100" width="100">
+          <use xlinkHref="#icon-r" />
+        </svg>
+        <svg height="100" width="100">
+          <use xlinkHref="#icon-o" />
+        </svg>
+        <svg height="100" width="100">
+          <use xlinkHref="#icon-x" />
+        </svg>
+        <svg height="100" width="100">
+          <use xlinkHref="#icon-b" />
+        </svg>
+        <svg height="100" width="100">
+          <use xlinkHref="#icon-u" />
+        </svg>
+        <svg height="100" width="100">
+          <use xlinkHref="#icon-r" />
+        </svg>
+        <svg height="100" width="100">
+          <use xlinkHref="#icon-y" />
+        </svg>
+      </div>
+      <div className="flex logo-poetry justify-center my-8">
+        <svg height="100" width="100">
+          <use xlinkHref="#icon-p" />
+        </svg>
+        <svg height="100" width="100">
+          <use xlinkHref="#icon-o" />
+        </svg>
+        <svg height="100" width="100">
+          <use xlinkHref="#icon-e" />
+        </svg>
+        <svg height="100" width="100">
+          <use xlinkHref="#icon-t" />
+        </svg>
+        <svg height="100" width="100">
+          <use xlinkHref="#icon-r" />
+        </svg>
+        <svg height="100" width="100">
+          <use xlinkHref="#icon-y" />
+        </svg>
+      </div>
+      <div className="flex logo-festival justify-center">
+        <svg height="100" width="100">
+          <use xlinkHref="#icon-f" />
+        </svg>
+        <svg height="100" width="100">
+          <use xlinkHref="#icon-e" />
+        </svg>
+        <svg height="100" width="100">
+          <use xlinkHref="#icon-s" />
+        </svg>
+        <svg height="100" width="100">
+          <use xlinkHref="#icon-t" />
+        </svg>
+        <svg height="100" width="100">
+          <use xlinkHref="#icon-i" />
+        </svg>
+        <svg height="100" width="100">
+          <use xlinkHref="#icon-v" />
+        </svg>
+        <svg height="100" width="100">
+          <use xlinkHref="#icon-a" />
+        </svg>
+        <svg height="100" width="100">
+          <use xlinkHref="#icon-l" />
+        </svg>
+      </div>
+    </div>
+  );
+}
