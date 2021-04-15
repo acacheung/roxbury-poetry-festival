9a93bbbc10c288e7570e54bdc57678c7ee1d5af6
Ashley Ramsay <nexus009@gmail.com>
2021-04-15 19:12:16 +1200

    mobile banner changes

--- components/Banner.js
+++ components/Banner.js
@@ -7,18 +7,10 @@ export default function Banner() {
     >
       <div className="banner-text bg-gray-900 flex h-full items-center justify-end">
         <p>Register Now</p>
-        <svg
-          className="fill-current hidden md:block ml-6"
-          height="12"
-          width="15"
-        >
+        <svg className="fill-current ml-4 md:ml-6" height="12" width="15">
           <use xlinkHref="#icon-rightarrow" />
         </svg>
-        <svg
-          className="fill-current hidden md:block ml-6"
-          height="12"
-          width="15"
-        >
+        <svg className="fill-current ml-4 md:ml-6" height="12" width="15">
           <use xlinkHref="#icon-rightarrow" />
         </svg>
         <svg className="fill-current mx-4 md:mx-6" height="12" width="15">
