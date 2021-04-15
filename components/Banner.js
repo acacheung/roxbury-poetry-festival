adf2fbbed739a74dbcc7e65ae8aa72d953cf6553
Ashley Ramsay <nexus009@gmail.com>
2021-04-15 19:23:55 +1200

    asdf

--- components/Banner.js
+++ components/Banner.js
@@ -3,6 +3,7 @@ export default function Banner() {
     <a
       className="background-banner bottom-0 fixed h-16 items-center z-50 text-white md:text-2xl uppercase w-full"
       href="https://www.eventbrite.ca/e/roxbury-poetry-festival-2021-tickets-146513542951"
+      rel="noreferrer noopener"
       target="_blank"
     >
       <div className="banner-text bg-gray-900 flex h-full items-center justify-end">
