75e43d65e405689c9d492cb31dcbdbf43d98e49f
Ashley Ramsay <nexus009@gmail.com>
2021-04-16 12:35:08 +1200

    fixes for errors and schedule hover states

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,6 @@ export default function Layout({ children }) {
   return (
     <>
       <Head>
-        <html lang="en" />
         <title>Roxbury Poetry Festival</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
